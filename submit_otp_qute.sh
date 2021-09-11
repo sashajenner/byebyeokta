b59e69f6b936decf3983a1c48408ee5c928d806d
Sasha Jenner <jenner.sasha@gmail.com>
2021-09-11 15:16:28 +1000

    checking input and qutebrowser command update

--- submit_otp_qute.sh
+++ submit_otp_qute.sh
@@ -8,7 +8,15 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-otp=$(pass otp "$1")
+OTP_NAME="$1"
+
+# retrieve the otp
+otp=$(pass otp "$OTP_NAME")
+
+if [ -z "$otp" ]; then
+    echo "Unknown OTP_NAME '$OTP_NAME'"
+    exit 1
+fi
 
 # insert the otp at the focussed text box and submit it
-echo "insert-text $otp;; enter-mode insert;; fake-key <Enter>" >> "$QUTE_FIFO"
+echo "insert-text $otp;; mode-enter insert;; fake-key <Enter>" >> "$QUTE_FIFO"
